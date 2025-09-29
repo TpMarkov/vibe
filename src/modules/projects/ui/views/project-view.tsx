c837ce517a118b11f439c7fd9c651560f6403cc9
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-09-29 09:23:28 +0300

    16 :Authentication

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -16,6 +16,7 @@ import { CodeIcon, CrownIcon, EyeIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { FileExplorer } from "@/components/file-explorer";
+import { UserControl } from "@/components/ui/user-control";
 interface Props {
   projectId: string;
 }
@@ -67,6 +68,7 @@ const ProjectView = ({ projectId }: Props) => {
                     <CrownIcon /> Upgrade
                   </Link>
                 </Button>
+                <UserControl showName={false} />
               </div>
             </div>
             <TabsContent value="preview">
